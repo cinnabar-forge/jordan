73ac06fc289a8a38e9dd8fa650ef2bcb1efee504
Timur Moziev <code@timurrin.ru>
2024-02-19 02:42:01 +0300

    init

---
+++ src/interactive.js
@@ -0,0 +1,5 @@
+// import inquirer from "inquirer";
+
+export function viewer() {
+  console.log("WARNING: There will be interactive shell only. To use Jordan in cli command mode enter 'jordan-command'");
+}
